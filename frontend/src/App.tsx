d8379bf51a3dbe258889b6108e53d5feeb7b02d7
Editorhacker <sutaraditya2807@gmail.com.com>
2026-02-14 01:51:25 +0530

    fix route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,27 +6,21 @@ import Analytics from './pages/Analytics';
 import OwnerDashboard from './pages/OwnerDashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 
+
 function App() {
   return (
     <Routes>
-
-      {/* Public */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
 
-      {/* Protected */}
+      {/* Protected Routes */}
       <Route element={<ProtectedRoute />}>
         <Route path="/employee/*" element={<EmployeeDashboard />} />
         <Route path="/analytic" element={<Analytics />} />
         <Route path="/owner" element={<OwnerDashboard />} />
       </Route>
 
-      {/* Default redirect */}
       <Route path="/" element={<Navigate to="/login" replace />} />
-
-      {/* Catch all */}
-      <Route path="*" element={<Navigate to="/login" replace />} />
-
     </Routes>
   );
 }
